fbd9bbf7f346ded75a44b07865bc8f4abf0b08e7
pthn09 <pthn2207@gmail.com>
2025-07-07 09:56:33 +0700

    Update Weird Algorithm.cpp

--- Introductory Problems/Weird Algorithm.cpp
+++ Introductory Problems/Weird Algorithm.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
-    int n;
+    long long n;
     cin>>n;
     while (n!=1){
         cout<<n<<' ';
